e017c7ddc4f5e57d473789539065f3d52d0c8449
Olivier Lépine <olivier@lepine.fr>
2018-03-05 17:29:21 +0100

    remove config file

--- mobile-app/src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDekZKMtEv12tNnxWBsMN1kLf4XhHUnTa4",
-  authDomain: "lux4-3d652.firebaseapp.com",
-  databaseURL: "https://lux4-3d652.firebaseio.com",
-  projectId: "lux4-3d652",
-  storageBucket: "",
-  messagingSenderId: "324288503121"
-};
